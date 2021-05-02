90082dd63d925691afc9483737ba378ce94d7c3b
Diego <soterg.uhdeia@gmail.com>
2021-05-02 18:23:10 -0500

    class component for geolocation API

--- src/index.js
+++ src/index.js
@@ -1,37 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ApprovalCard from "./ApprovalCard";
-import CommentDetail from "./CommentDetail";
 
-const App = () => {
-  return (
-    <div className="ui container comments">
-      <ApprovalCard>
-        <CommentDetail
-          imgID="50"
-          author="Sam"
-          timeAgo="Today at 4:45 pm"
-          comment="That sounds awesome!"
-        />
-      </ApprovalCard>
-      <ApprovalCard>
-        <CommentDetail
-          imgID="500"
-          author="Carlos"
-          timeAgo="Today at 3:15 pm"
-          comment="sounds cool bro"
-        />
-      </ApprovalCard>
-      <ApprovalCard>
-        <CommentDetail
-          imgID="130"
-          author="Juana"
-          timeAgo="Today at 6:26 am"
-          comment="rad my man"
-        />
-      </ApprovalCard>
-    </div>
-  );
-};
+class App extends React.Component {
+
+  render(){
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.error(err)
+    )
+
+    return (
+      <div>Latitude: </div>
+    );
+  }
+} 
 
 ReactDOM.render(<App />, document.getElementById("root"));
