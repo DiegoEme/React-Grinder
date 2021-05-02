5b65ff43b351c2e4a6487f615f7eef6996272e03
Diego <soterg.uhdeia@gmail.com>
2021-05-02 17:20:43 -0500

    props

---
+++ src/CommentDetail.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const CommentDetail = ({author, timeAgo, comment, imgID}) => {
+    
+  return (
+    <div className="comment">
+      <a href="/" className="avatar">
+        <img src={`https://picsum.photos/id/${imgID}/200`} alt="avatar" />
+      </a>
+      <div className="content">
+        <a href="/" className="author">
+          {author}
+        </a>
+        <div className="metadata">
+          <span className="date">{timeAgo}</span>
+        </div>
+        <div className="text">{comment}</div>
+      </div>
+    </div>
+  );
+};
+
+export default CommentDetail;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import ApprovalCard from "./ApprovalCard";
+import CommentDetail from "./CommentDetail";
 
 const App = () => {
-    return <div>Hi there!</div>;
-}
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <CommentDetail
+          imgID="50"
+          author="Sam"
+          timeAgo="Today at 4:45 pm"
+          comment="That sounds awesome!"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          imgID="500"
+          author="Carlos"
+          timeAgo="Today at 3:15 pm"
+          comment="sounds cool bro"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          imgID="130"
+          author="Juana"
+          timeAgo="Today at 6:26 am"
+          comment="rad my man"
+        />
+      </ApprovalCard>
+    </div>
+  );
+};
 
-ReactDOM.render(
-    <App />,
-    document.getElementById("root")
-)
+ReactDOM.render(<App />, document.getElementById("root"));
