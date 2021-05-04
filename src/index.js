8a4f4b0347c830ab4b48b99ecee5847c1c05536e
Diego <soterg.uhdeia@gmail.com>
2021-05-04 16:57:32 -0500

    starting new app with user events and input

--- src/index.js
+++ src/index.js
@@ -1,46 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import SeasonDisplay from "./SeasonDisplay";
-import Spinner from "./Spinner"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
 
-class App extends React.Component {
-  constructor(props){
-    super(props);//es una referencia al componente padre React.component
-    
-    this.state = {lat: null, error: ""};    
-  }
-
-  componentDidMount(){
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({lat: position.coords.latitude})
-      },
-      (err) => {
-        this.setState({error: err.message})
-      }
-    )
-  }
-
-  //Always try to not have more than one return in the render method
-  renderContent(){
-    if(this.state.error && !this.state.lat){
-      return <div>Error: {this.state.error}</div>
-    }
-    if(!this.state.error && this.state.lat){
-      return <SeasonDisplay lat={this.state.lat}/>
-    }
-    return <Spinner message="Please accept location request"/>
-  }
-
-  //We always have to define render!
-  render(){   
-    return (
-      <div>
-        {this.renderContent()}
-      </div>
-    )
-    
-  }
-} 
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"))
