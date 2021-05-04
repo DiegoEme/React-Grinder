873a6c647f893b70ac7ef16903a08f78973b8a79
Diego <soterg.uhdeia@gmail.com>
2021-05-03 20:57:54 -0500

    working with state and class components

--- src/index.js
+++ src/index.js
@@ -2,17 +2,32 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-
-  render(){
+  constructor(props){
+    super(props);//es una referencia al componente padre React.component
+    
+    this.state = {lat: null, error: ""};
 
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.error(err)
+      (position) => {
+        this.setState({lat: position.coords.latitude})
+      },
+      (err) => {
+        this.setState({error: err.message})
+      }
     )
+  }
 
-    return (
-      <div>Latitude: </div>
-    );
+  //We always have to define render!
+  render(){
+    
+     if(this.state.error && !this.state.lat){
+       return <div>Error: {this.state.error}</div>
+     }
+     if(!this.state.error && this.state.lat){
+       return <div>Latitude: {this.state.lat}</div>
+     }
+     return <div>Loading...</div>
+    
   }
 } 
 
