8fe10ad4f6d9bcbaa7723f6408a70c389e642c3d
Diego <soterg.uhdeia@gmail.com>
2021-05-04 21:34:20 -0500

    passing props up to parent and fetching data with axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
-const  App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    )
+class  App extends React.Component {
+    onSearchSubmit(term){
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: {query: term},
+            headers: {
+                Authorization: 'Client-ID gCIXIBLBwsfSypwzUNneI1ZH-SftGBseatx6M_IFVck'
+            }
+        })
+    }
+
+    render() {
+        return(
+
+            <div className="ui container">
+                <SearchBar busqueda={this.onSearchSubmit} />
+            </div>
+        )
+    }
 }
 
 export default App
